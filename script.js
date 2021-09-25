86c6260828452bb8fec26797038f9c279cc7ebbd
liangtcode <liangt.code@gmail.com>
2021-09-25 11:31:11 +0800

    cleaned up code with emojis and suggestion

--- script.js
+++ script.js
@@ -1,276 +1,7 @@
-/*
-// Initialise the card deck representation as an array of objects
-var deck = [
-  {
-    name: "ace",
-    suit: "hearts",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "hearts",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "hearts",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "hearts",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "hearts",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "hearts",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "hearts",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "hearts",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "hearts",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "hearts",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "hearts",
-    rank: 10,
-  },
-  {
-    name: "queen",
-    suit: "hearts",
-    rank: 10,
-  },
-  {
-    name: "king",
-    suit: "hearts",
-    rank: 10,
-  },
-  {
-    name: "ace",
-    suit: "diamonds",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "diamonds",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "diamonds",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "diamonds",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "diamonds",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "diamonds",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "diamonds",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "diamonds",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "diamonds",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "diamonds",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "diamonds",
-    rank: 10,
-  },
-  {
-    name: "queen",
-    suit: "diamonds",
-    rank: 10,
-  },
-  {
-    name: "king",
-    suit: "diamonds",
-    rank: 10,
-  },
-  {
-    name: "ace",
-    suit: "clubs",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "clubs",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "clubs",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "clubs",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "clubs",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "clubs",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "clubs",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "clubs",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "clubs",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "clubs",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "clubs",
-    rank: 10,
-  },
-  {
-    name: "queen",
-    suit: "clubs",
-    rank: 10,
-  },
-  {
-    name: "king",
-    suit: "clubs",
-    rank: 10,
-  },
-  {
-    name: "ace",
-    suit: "spades",
-    rank: 1,
-  },
-  {
-    name: "2",
-    suit: "spades",
-    rank: 2,
-  },
-  {
-    name: "3",
-    suit: "spades",
-    rank: 3,
-  },
-  {
-    name: "4",
-    suit: "spades",
-    rank: 4,
-  },
-  {
-    name: "5",
-    suit: "spades",
-    rank: 5,
-  },
-  {
-    name: "6",
-    suit: "spades",
-    rank: 6,
-  },
-  {
-    name: "7",
-    suit: "spades",
-    rank: 7,
-  },
-  {
-    name: "8",
-    suit: "spades",
-    rank: 8,
-  },
-  {
-    name: "9",
-    suit: "spades",
-    rank: 9,
-  },
-  {
-    name: "10",
-    suit: "spades",
-    rank: 10,
-  },
-  {
-    name: "jack",
-    suit: "spades",
-    rank: 10,
-  },
-  {
-    name: "queen",
-    suit: "spades",
-    rank: 10,
-  },
-  {
-    name: "king",
-    suit: "spades",
-    rank: 10,
-  },
-];
-*/
-
 // Initialise static variables for game status
 var GAME_STATUS_SHUFFLE_DECK = "SHUFFLE DECK";
-var GAME_STATUS_INITIAL_DEAL = "INITIAL DEAL";
 var GAME_STATUS_DISPLAY_CARDS = "DISPLAY CARDS";
 var GAME_STATUS_HUMAN_DECIDES = "HUMAN DECIDES";
-var GAME_STATUS_HUMAN_STANDS = "HUMAN STANDS";
-var GAME_STATUS_HUMAN_HITS = "HUMAN HITS";
 var GAME_STATUS_RESULTS = "RESULTS";
 
 // Initialise static variables for hit or stand
@@ -291,6 +22,21 @@ var makeDeck = function () {
   // Intialise an arrage of the 4 suits in our deck. We will loop over this array.
   var suits = ["hearts", "diamonds", "clubs", "spades"];
   var suitEmoji = ["♣️", "♠️", "♦️", "♥️"];
+  var nameEmoji = [
+    "A",
+    "2️⃣",
+    "3️⃣",
+    "4️⃣",
+    "5️⃣",
+    "6️⃣",
+    "7️⃣",
+    "9️⃣",
+    "8️⃣",
+    "🔟",
+    "🕺🏽",
+    "👸🏽",
+    "🤴🏽",
+  ];
 
   // Loop over the suits array
   var suitIndex = 0;
@@ -307,6 +53,7 @@ var makeDeck = function () {
       // By default, the card name is the same as rankCounter
       var cardName = rankCounter;
       var currentRank = rankCounter;
+      var currentNameEmoji = nameEmoji[rankCounter - 1];
 
       // if rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
       if (cardName == 1) {
@@ -328,6 +75,7 @@ var makeDeck = function () {
         suit: currentSuit,
         rank: currentRank,
         emoji: currentEmoji,
+        nameEmoji: currentNameEmoji,
       };
 
       // Add the new card to the deck
@@ -432,7 +180,7 @@ var checkBust = function (playerCards) {
   return ifBust;
 };
 
-// check winner
+// get which player has higher card sum
 var getWinner = function (humanCards, dealerCards) {
   var winner = "";
   var humanCardSum = cardSum(humanCards);
@@ -460,11 +208,13 @@ var displayCards = function (humanCards, dealerCards) {
       msg =
         msg +
         `Player card ${humanCardsCounter + 1}: ${
-          humanCards[humanCardsCounter].name
+          humanCards[humanCardsCounter].nameEmoji
         } of ${humanCards[humanCardsCounter].emoji}. <br>`;
 
       humanCardsCounter += 1;
     }
+
+    msg = msg + "<br>";
 
     var dealerCardsCounter = 0;
     // add to message for all dealer cards
@@ -472,33 +222,83 @@ var displayCards = function (humanCards, dealerCards) {
       msg =
         msg +
         `Dealer card ${dealerCardsCounter + 1}: ${
-          dealerCards[dealerCardsCounter].name
+          dealerCards[dealerCardsCounter].nameEmoji
         } of ${dealerCards[dealerCardsCounter].emoji}. <br>`;
 
       dealerCardsCounter += 1;
     }
-    // add player and dealer totals to message
-    msg =
-      msg +
-      `Player total is ${cardSum(humanCards)}. Dealer total is ${cardSum(
-        dealerCards
-      )} <br>`;
+
+    msg = msg + "<br>";
+
+    // if human only has two cards, check for Black Jack
+    if (humanCards.length == 2) {
+      if (checkBlackJack(humanCards)) {
+        msg =
+          msg +
+          `Player hits Black Jack. PLAYER WINS. <br> Player total is ${cardSum(
+            humanCards
+          )}. Dealer total is ${cardSum(
+            dealerCards
+          )}. <br><br> Click submit to play again.`;
+        gameStatus = GAME_STATUS_SHUFFLE_DECK;
+        return msg;
+        // else check if dealer bust
+      } else if (checkBust(dealerCards)) {
+        msg =
+          msg +
+          `Dealer busts. PLAYER WINS. <br> Player total is ${cardSum(
+            humanCards
+          )}. Dealer total is ${cardSum(
+            dealerCards
+          )}. <br><br>Click submit to play again.`;
+        gameStatus = GAME_STATUS_SHUFFLE_DECK;
+        return msg;
+      }
+      // else if human has more than two cards, check if human busts
+    } else if (checkBust(humanCards)) {
+      msg =
+        msg +
+        `Player busts. DEALER WINS. <br> Player total is ${cardSum(
+          humanCards
+        )}. Dealer total is ${cardSum(
+          dealerCards
+        )}. <br><br>Click submit to play again.`;
+      gameStatus = GAME_STATUS_SHUFFLE_DECK;
+      return msg;
+      // else check if dealer busts
+    } else if (checkBust(dealerCards)) {
+      msg =
+        msg +
+        `Dealer busts. PLAYER WINS. <br> Player total is ${cardSum(
+          humanCards
+        )}. Dealer total is ${cardSum(
+          dealerCards
+        )}. <br><br>Click submit to play again.`;
+      gameStatus = GAME_STATUS_SHUFFLE_DECK;
+      return msg;
+    }
 
     // Initialise winner
     var winner = getWinner(humanCards, dealerCards);
 
     // add to output message based on winner
     if (winner == "human") {
-      msg = msg + "Player wins! <br>";
+      msg = msg + "PLAYER WINS! <br>";
     }
     if (winner == "dealer") {
-      msg = msg + "Dealer wins! <br>";
+      msg = msg + "DEALER WINS! <br>";
     }
     if (winner == "tie") {
-      msg = msg + "It's a tie. <br>";
+      msg = msg + "IT'S A TIE. <br>";
     }
 
-    msg = msg + "Press submit to play again!";
+    // add player and dealer totals to message
+    msg =
+      msg +
+      `Player total is ${cardSum(humanCards)}. Dealer total is ${cardSum(
+        dealerCards
+      )} <br><br>Click submit to play again!`;
+
     gameStatus = GAME_STATUS_SHUFFLE_DECK;
 
     // else for all other game statuses, create message to display all human cards plus first card of dealer
@@ -509,7 +309,7 @@ var displayCards = function (humanCards, dealerCards) {
       msg =
         msg +
         `Player card ${humanCardsCounter + 1}: ${
-          humanCards[humanCardsCounter].name
+          humanCards[humanCardsCounter].nameEmoji
         } of ${humanCards[humanCardsCounter].emoji}. <br>`;
 
       humanCardsCounter += 1;
@@ -517,8 +317,23 @@ var displayCards = function (humanCards, dealerCards) {
     // add dealer's first card to message
     msg =
       msg +
-      `Dealer card 1: ${dealerCards[0].name} of ${dealerCards[0].emoji}. <br> Type h for hit or s for stand.`;
+      `<br> Dealer card 1: ${dealerCards[0].nameEmoji} of ${dealerCards[0].emoji}.`;
+
+    // give hint to human
+    msg = msg + `<br><br> Player current total is ${cardSum(humanCards)}.`;
+    if (cardSum(humanCards) < 13) {
+      msg = msg + " You should hit.";
+    } else if (cardSum(humanCards) > 12 && cardSum(humanCards) < 17) {
+      msg = msg + " Tricky! Think about about you want to hit or stand.";
+    } else if (cardSum(humanCards > 16) && cardSum(humanCards < 20)) {
+      msg = msg + " You should probably stand.";
+    } else {
+      msg = msg + " You should stand.";
+    }
+
+    msg = msg + "<br><br> Type h for hit or s for stand.";
   }
+
   return msg;
 };
 
@@ -549,81 +364,47 @@ var main = function (input) {
       dealerCards[counter] = shuffledDeck.pop();
       counter += 1;
     }
-    console.log("Human cards are:");
-    console.log(humanCards);
-    console.log("Dealer cards are:");
-    console.log(dealerCards);
 
-    gameStatus = GAME_STATUS_DISPLAY_CARDS;
-
-    myOutputValue = "Deck shuffled. Press submit to deal cards.";
-  } else if (gameStatus == GAME_STATUS_DISPLAY_CARDS) {
-    myOutputValue = displayCards(humanCards, dealerCards);
-
-    // if human cards hit black jack
-    if (checkBlackJack(humanCards) == true) {
-      myOutputValue =
-        myOutputValue +
-        "Player hits Black Jack. Player wins. <br> Press submit to play again.";
+    if (checkBlackJack(humanCards)) {
+      gameStatus = GAME_STATUS_RESULTS;
+      myOutputValue = displayCards(humanCards, dealerCards);
       gameStatus = GAME_STATUS_SHUFFLE_DECK;
-      console.log(myOutputValue);
-      return myOutputValue;
-    } else if (checkBust(humanCards) == true) {
-      myOutputValue =
-        myOutputValue +
-        "Player busts. Dealer wins. <br> Press submit to play again.";
-      gameStatus = GAME_STATUS_SHUFFLE_DECK;
-      console.log(myOutputValue);
-      return myOutputValue;
     } else {
-      myOutputValue = myOutputValue + "Type h for hit or s for stand.";
+      gameStatus = GAME_STATUS_DISPLAY_CARDS;
+      myOutputValue = displayCards(humanCards, dealerCards);
       gameStatus = GAME_STATUS_HUMAN_DECIDES;
     }
-
-    // else human does not hit black jack
-    //     } else if (checkBust(humanCards == true) {
-    // else if (checkBust(humanCards == true) {
-    // myOutputValue =
-    //         myOutputValue +
-    //         "Player busts. Dealer wins. <br> Press submit to play again.";
-    //       gameStatus = GAME_STATUS_SHUFFLE_DECK;
-    //     }
-    //     }
-
-    //     }
-    // else  {
-    //   myOutputValue = myOutputValue + "Type h for hit or s for stand.";
-
-    //   gameStatus = GAME_STATUS_HUMAN_DECIDES;
-    // }
   } else if (gameStatus == GAME_STATUS_HUMAN_DECIDES) {
+    // check input to make sure it's "h" or "s"
+    if (input !== HIT && input !== STAND) {
+      myOutputValue = "Please input h for hit or s for stand";
+      return myOutputValue;
+    }
+    // if human decides to hit
     if (input == HIT) {
-      gameStatus = GAME_STATUS_HUMAN_HITS;
-      myOutputValue = "Player hits. Press submit to be dealt another card.";
+      // deal another card to human
+      humanCards.push(shuffledDeck.pop());
+      // check if human busts
+      if (checkBust(humanCards)) {
+        gameStatus = GAME_STATUS_RESULTS;
+        myOutputValue = displayCards(humanCards, dealerCards);
+        gameStatus = GAME_STATUS_SHUFFLE_DECK;
+      } else {
+        gameStatus = GAME_STATUS_DISPLAY_CARDS;
+        myOutputValue = displayCards(humanCards, dealerCards);
+        gameStatus = GAME_STATUS_HUMAN_DECIDES;
+      }
+      // if human decides to stand
     } else if (input == STAND) {
-      gameStatus = GAME_STATUS_HUMAN_STANDS;
-      myOutputValue = "Player stands. Dealer will deal own cards.";
+      // deal out cards for dealer while dealer cards sum is less than 17
+      while (isBelow17(dealerCards)) {
+        dealerCards.push(shuffledDeck.pop());
+      }
+      // set game status to GAME_STATUS_RESULTS
+      gameStatus = GAME_STATUS_RESULTS;
+      myOutputValue = displayCards(humanCards, dealerCards);
     }
-  } else if (gameStatus == GAME_STATUS_HUMAN_STANDS) {
-    // deal cards for dealer
-
-    while (isBelow17(dealerCards)) {
-      dealerCards.push(shuffledDeck.pop());
-    }
-
-    // set game status to Results
-    gameStatus = GAME_STATUS_RESULTS;
-    // if human decides to stand then display all human and dealer cards
-    myOutputValue = displayCards(humanCards, dealerCards);
-  } else if (gameStatus == GAME_STATUS_HUMAN_HITS) {
-    // if human decides to hits then deal one more card for human
-    humanCards.push(shuffledDeck.pop());
-    myOutputValue = displayCards(humanCards, dealerCards);
-    gameStatus = GAME_STATUS_HUMAN_DECIDES;
   }
-
-  console.log("end game status:");
-  console.log(gameStatus);
 
   return myOutputValue;
 };
